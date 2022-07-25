8c2a22d96b90e8a66091c1a5e0cbfcd9043e5c94
mdsujan-mridha <mdsujan134523@gmail.com>
2022-07-25 20:51:18 +0600

    all done

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(express.json());
 app.use(cors());
 
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zlz6i.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -21,13 +19,19 @@ async function run(){
     try{
        await client.connect();
        const toDoItemsCollection = client.db("todoitems").collection("items");
+       const userCollection = client.db('users').collection('user');
 
        app.get('/items', async(req,res)=>{
             const query = {};
             const cursor = toDoItemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
-       })
+       });
+      //  get user from database 
+      app.get('/users', async (req, res) => {
+        const users = await userCollection.find().toArray()
+        res.send(users);
+    });
 
        app.post('/items', async(req,res)=>{
          
@@ -36,6 +40,21 @@ async function run(){
         res.send(result);
 
        });
+
+
+      // post user in database 
+      app.post('/users/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: user,
+        };
+        const result = await userCollection.updateOne(filter, updateDoc, options);
+        res.send({ result});
+    });
+
        app.put('/items/:id' ,async(req, res)=>{
          const id = req.params.id;
          const updateItem = req.body;
